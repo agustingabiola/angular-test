1cc2eb682f730e7e94932c1f9ef99d2162a9a7d3
agustingabiola <agustingabiola@users.noreply.github.com>
2024-09-02 16:09:21 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
